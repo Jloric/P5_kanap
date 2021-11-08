12e3e5daf8d2fb14a3c4dccb8ea9e8661b3da343
Loric Minatchy <minatchyjloric@gmail.com>
2021-11-08 22:19:49 +0100

    change

--- P5-Minatchy_code_fonctionnel/front/Js/confirmation.js
+++
@@ -1,5 +0,0 @@
-let order = new URLSearchParams(window.location.search);
-let orderId = order.get('id');
-console.log(orderId)
-//on récupère l'id dans l'URL que l'on affiche sur la page
-document.getElementById('orderId').innerHTML = ''+orderId+'';
